2943b2834e0f39c9b7d564f0321abd553da4db6f
Louisa Smith <louisahsmith@gmail.com>
2023-08-13 21:37:36 -0400

    update day 1

--- create-pdfs.R
+++ create-pdfs.R
@@ -1,2 +1,8 @@
-list.files("_site/slides", pattern = "*.html", full.names = TRUE) |>
-  purrr::walk(renderthis::to_pdf)
+
+# 
+# list.files("_site/day-1/slides", pattern = "*.html", full.names = TRUE) |>
+#   purrr::walk(renderthis::to_pdf)
+# 
+# pdftools::pdf_combine(list.files("_site/day-1/slides", pattern = "*.pdf", full.names = TRUE),
+#             output = "day-1/slides/day-1-slides.pdf")
+
