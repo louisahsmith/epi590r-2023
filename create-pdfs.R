9d7cae327903fa7d3d0418a2d44aa7efcb5d584d
Louisa Smith <louisahsmith@gmail.com>
2023-08-17 12:17:43 -0400

    info about final

--- create-pdfs.R
+++ create-pdfs.R
@@ -5,10 +5,10 @@
 # 
 # pdftools::pdf_combine(list.files("_site/day-1/slides", pattern = "*.pdf", full.names = TRUE),
 #             output = "day-1/slides/day-1-slides.pdf")
-
-list.files("_site/day-2/slides", pattern = "*.html", full.names = TRUE) |>
-  purrr::walk(renderthis::to_pdf)
-
-pdftools::pdf_combine(list.files("_site/day-2/slides", pattern = "*.pdf", full.names = TRUE),
-            output = "day-2/slides/day-2-slides.pdf")
+# 
+# list.files("_site/day-2/slides", pattern = "*.html", full.names = TRUE) |>
+#   purrr::walk(renderthis::to_pdf)
+# 
+# pdftools::pdf_combine(list.files("_site/day-2/slides", pattern = "*.pdf", full.names = TRUE),
+#             output = "day-2/slides/day-2-slides.pdf")
 
